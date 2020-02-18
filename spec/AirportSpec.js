e88a2c1c54400b4a4b256e6e5e2131292351c5a8
Liz Daly <liz7daly@gmail.com>
2020-02-18 14:35:08 +0000

    Add plane and airport
    Passes feature test: plane can land at an airport.

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -2,10 +2,17 @@
 
 describe('Airport', function() {
   var airport;
+  var plane;
   beforeEach(function() {
-    airport = new airport();
+    airport = new Airport();
+    plane = jasmine.createSpy('plane',['land']);
   });
   it ('can keep track of planes', function(){
     expect(airport.planes()).toEqual([])
+  });
+
+  it ('can land a plane', function(){
+    airport.clearForLanding(plane)
+    expect(airport.planes()).toEqual([plane])
   });
 });
--- src/Airport.js
+++ src/Airport.js
@@ -1,4 +1,13 @@
 'use strict';
 
-function Airport() {} 
-Airport.prototype.planes = function(){return []; };
+function Airport() {
+  this._hangar = []
+};
+
+Airport.prototype.planes = function(){
+  return this._hangar
+ };
+
+Airport.prototype.clearForLanding = function(plane) {
+  return this._hangar.push(plane)
+};
